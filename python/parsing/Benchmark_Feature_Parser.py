d6e30ae91df9f2bc418dd3e14d9939d5f1dda8d2
libuser <libuser@mac8.union.edu>
2019-04-10 01:41:33 -0400

    altered feature parser to build each feature dict separately. DRAMATIC improvement.

--- python/parsing/Benchmark_Feature_Parser.py
+++ python/parsing/Benchmark_Feature_Parser.py
@@ -3,73 +3,83 @@
 # Author    : Nathan Oasis              #
 #########################################
 
-# Importing library
-import os
-import tempfile
-
-# Vars
-# SEARCH_ON_ID = 0 # search on MSD id
-SEARCH_ON_ID = 1 # search on spotify id
-
 # Paths
-
-error_id_path = '/Users/libuser/Desktop/OasisThesis/OasisThesisDownloads/Building_CSV/no_structal_features.csv'
+# error_id_path = '/Users/libuser/Desktop/OasisThesis/OasisThesisDownloads/Building_CSV/no_structal_features.csv'
 
 low_level_feature_dir = '/Users/libuser/Desktop/OasisThesis/OasisThesisDownloads/MSD Benchmarks/CSV/Low-Level_Features/'
-low_level_feature_paths = [low_level_feature_dir+file for file in os.listdir(low_level_feature_dir) if file.endswith(".csv")]
-
-timbral_features = ['/Users/libuser/Desktop/OasisThesis/OasisThesisDownloads/MSD Benchmarks/CSV/Timbral_Features/Marsyas_Timbral.csv']
-
 mid_level_feature_dir = '/Users/libuser/Desktop/OasisThesis/OasisThesisDownloads/MSD Benchmarks/CSV/Rhythm_Features/'
-mid_level_feature_file1 = 'Rhythm_Histograms.csv'
-mid_level_feature_file2 = 'Statistical_Spectrum_Descriptors.csv'
-mid_level_feature_paths = [mid_level_feature_dir+mid_level_feature_file1, mid_level_feature_dir+mid_level_feature_file2]
+
+AoM = low_level_feature_dir + 'Area_of_Moments.csv'
+MoM = low_level_feature_dir + 'Method_of_Moments.csv'
+LPC = low_level_feature_dir + 'Linear_Predictive_Coding.csv'
+MFCC = low_level_feature_dir + 'MFCC.csv'
+Spec_All = low_level_feature_dir + 'Spectral_All.csv'
+Spec_All_Deriv = low_level_feature_dir + 'Spectral_All_Derivatives.csv'
+Marsyas = low_level_feature_dir + 'Marsyas_Timbral.csv'
+RH = mid_level_feature_dir + 'Rhythm_Histograms.csv'
+SSD = mid_level_feature_dir + 'Statistical_Spectrum_Descriptors.csv'
+Spotify = '/Users/libuser/Desktop/OasisThesis/OasisThesisDownloads/Building_CSV/spotify_features.csv'
+feature_dicts = {AoM: {}, MoM: {}, LPC: {}, MFCC: {}, Spec_All: {}, Spec_All_Deriv: {}, Marsyas: {}, RH: {}, SSD: {}, Spotify: {}}
 
 
-master_cvs_IN_PATH = '/Users/libuser/Desktop/OasisThesis/OasisThesisDownloads/Building_CSV/Master_2.0.csv'
-master_cvs_OUT_PATH = '/Users/libuser/Desktop/OasisThesis/OasisThesisDownloads/Building_CSV/Master_2.1.csv'
+out_dir = '/Users/libuser/Desktop/OasisThesis/OasisThesisDownloads/Building_CSV/out_files/'
+RH_SSD = 'RH_SSD.csv'
+RH_SSD_jMir_deriv = 'RH_SSD_jMir_deriv.csv'
+RH_SSD_jMir_noderiv = 'RH_SSD_jMir_noderiv.csv'
+RH_SSD_MARSYAS = 'RH_SSD_MARSYAS.csv'
+out_files = {out_dir + RH_SSD: {SSD, RH}, out_dir + RH_SSD_jMir_noderiv: {SSD, RH, AoM, MoM, LPC, MFCC, Spec_All},
+             out_dir + RH_SSD_jMir_deriv: {SSD, RH, AoM, MoM, LPC, MFCC, Spec_All_Deriv}, out_dir + RH_SSD_MARSYAS: {SSD, RH, Marsyas}}
 
-feature_paths = ['/Users/libuser/Desktop/OasisThesis/OasisThesisDownloads/Building_CSV/spotify_features.csv']
-# feature_paths = []
-# feature_paths.extend(low_level_feature_paths)
-# feature_paths.extend(mid_level_feature_paths)
-# feature_paths.extend(timbral_features)
+master_cvs_IN_PATH = '/Users/libuser/Desktop/OasisThesis/OasisThesisDownloads/Building_CSV/IDs_and_Genre.csv'
 
-track_ids_included = set()
+msd_track_ids_included = set()
+spotify_track_ids_included = set()
 with open(master_cvs_IN_PATH, "r") as master_IN:
     for line in master_IN:
-        id = line.split(',')[SEARCH_ON_ID] # spotify is 1. track is 0.
-        track_ids_included.add(id)
-    print(len(track_ids_included))
+        split = line.split(',')  # spotify is 1. track is 0.
+        msd_id = split[0]
+        spotify_id = split[1]
+        msd_track_ids_included.add(msd_id)
+        spotify_track_ids_included.add(spotify_id)
 
-i = 0
+for feature in feature_dicts:
+    print('Working on building dict for feature file', feature)
+    with open(feature, 'r') as feature_file:
+        for line in feature_file:
+            line = line.rstrip().rstrip(',').split(',')
+            feature_id = line[-1].lstrip('\'').rstrip('\'')
 
-with open(error_id_path, 'w') as errorFile:
-    for feature_path in feature_paths:
-        print('Working on feature file', feature_path)
+            if 'spotify' not in feature:
+                if feature_id in msd_track_ids_included:
+                    feature_dicts[feature][feature_id] = ','.join(line[:-1])
+            else:
+                if feature_id in spotify_track_ids_included:
+                    feature_dicts[feature][feature_id] = ','.join(line[:-1])
 
-        feature_map = {}
-        with open(feature_path, "r") as feature_file:
-            for line in feature_file:
-                line = line.rstrip().rstrip(',').split(',')
-                feature_id = line[-1].lstrip('\'').rstrip('\'')
-                if feature_id in track_ids_included:
-                    feature_map[feature_id] = ','.join(line[:-1])
+with open(master_cvs_IN_PATH, "r") as master_IN:
+    for out_file in out_files:
+        included_feature_paths = out_files[out_file]
 
-        with open(master_cvs_IN_PATH, "r") as master_IN:
-            with open(master_cvs_OUT_PATH, "w") as outFile:
-                for line_in in master_IN:
-                    line_in = line_in.split(',')
-                    line_id = line_in[SEARCH_ON_ID]
-                    if line_id in feature_map:
-                        feature_line = feature_map[line_id]
-                        first_bit = ','.join(line_in[:-1])
-                        genre = line_in[-1]
-                        combined_line = first_bit + ',' + feature_line + ',' + genre
-                        outFile.write(combined_line)
-                    else:
-                        errorFile.write(line_id + '\n')
+        with open(out_file, "w") as the_out_file:
+            print('writing to', the_out_file)
+            exceptions = 0
+            for line in master_IN:
+                line = line.split(',')
+                msd_id = line[0]
+                spotify_id = line[1]
 
-        master_cvs_IN_PATH = master_cvs_OUT_PATH
-        master_cvs_OUT_PATH = master_cvs_OUT_PATH[:-4] + str(i) + '.csv'
-        i += 1
+                idstxt = ','.join(line[:-1])
+                genre = line[-1]
+
+                feature_str = ''
+                try:
+                    for feature_key in included_feature_paths:
+                        feature_str += feature_dicts[feature_key][msd_id]
+                    spot_features = feature_dicts[Spotify][spotify_id]
+                    s = idstxt + ',' + feature_str + ',' + spot_features + ',' + genre
+                    the_out_file.write(s)
+                except:
+                    exceptions += 1
+                    pass  # could write to error file here...
+            print('found', exceptions, 'exceptions')
+            master_IN.seek(0)
