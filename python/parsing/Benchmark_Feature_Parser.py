4d7bacd987a972f4999c4fa9d4fbbf900e8ca306
libuser <libuser@mac8.union.edu>
2019-04-10 10:22:32 -0400

    fixed comma bug

--- python/parsing/Benchmark_Feature_Parser.py
+++ python/parsing/Benchmark_Feature_Parser.py
@@ -74,7 +74,7 @@ with open(master_cvs_IN_PATH, "r") as master_IN:
                 feature_str = ''
                 try:
                     for feature_key in included_feature_paths:
-                        feature_str += feature_dicts[feature_key][msd_id]
+                        feature_str += (feature_dicts[feature_key][msd_id] + ',')
                     spot_features = feature_dicts[Spotify][spotify_id]
                     s = idstxt + ',' + feature_str + ',' + spot_features + ',' + genre
                     the_out_file.write(s)
--- python/parsing/fixed_genre_size.py
+++ python/parsing/fixed_genre_size.py
@@ -1,6 +1,7 @@
 songs_per_genre = 500
 
-dir = '/home/oasisn/Desktop/Thesis_CSVs/'
+# dir = '/home/oasisn/Desktop/Thesis_CSVs/'
+dir = '/Users/libuser/Desktop/OasisThesis/OasisThesisDownloads/Building_CSV/out_files/'
 RH_SSD = 'RH_SSD.csv'
 RH_SSD_jMir_deriv = 'RH_SSD_jMir_deriv.csv'
 RH_SSD_jMir_noderiv = 'RH_SSD_jMir_noderiv.csv'
@@ -22,20 +23,18 @@ with open(files[0], 'r') as in_file:
             if cnt_genre < 500:
                 genre_dict[genre].add(msd_id)
 
-msd_ids = {x for v in genre_dict.itervalues() for x in v}
+msd_ids = {x for v in genre_dict.values() for x in v}
 print(len(msd_ids))
 id_len = len('TRTQFGC128F422F013')
 
 
 for feature_file in files:
     with open(feature_file, 'r') as in_file:
-        with open(dir + 'eq_genre'+feature_file[33:], 'w+') as out_file:
+        with open(dir + 'eq_genre'+feature_file[len(dir):], 'w+') as out_file:
             for i, line in enumerate(in_file):
                 if i == 0:
                     out_file.write(line)
                 else:
                     msd_id = line[:id_len]
-                    # if i < 10:
-                    #     print(msd_id)
                     if msd_id in msd_ids:
                         out_file.write(line)
