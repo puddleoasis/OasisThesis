a0dd1ed182486a5aa43810e0529b78ffdf27572e
Nathan Oasis <oasisn@sh209-04.union.edu>
2019-04-10 14:57:28 -0400

    column parser

--- python/parsing/misc/swap_order.py
+++ python/parsing/misc/swap_order.py
@@ -1,11 +1,22 @@
-file_path = '/Users/libuser/PycharmProjects/OasisThesis/text/mergedSpotifyandTrackIDs.csv'
-file_out_path = '/Users/libuser/PycharmProjects/OasisThesis/text/mergedSpotifyandTrackIDs2.csv'
+dir = '/home/oasisn/Desktop/Thesis_CSVs/'
+RH_SSD = 'eq_genreRH_SSD.csv'
+RH_SSD_jMir_deriv = 'eq_genreRH_SSD_jMir_deriv.csv'
+RH_SSD_jMir_noderiv = 'eq_genreRH_SSD_jMir_noderiv.csv'
+RH_SSD_MARSYAS = 'eq_genreRH_SSD_MARSYAS.csv'
 
-with open(file_path, 'r') as inFile:
-    with open(file_out_path, 'w') as outFile:
-        for i, line in enumerate(inFile):
-            line = line.rstrip()[:-1].split(',')
-            a = line[0]
-            b = ','.join(line[1:-1])
-            c = line[-1]
-            outFile.write(a + ',' + c + ',' + b + '\n')
+files = [dir + RH_SSD_jMir_deriv, dir + RH_SSD_jMir_noderiv, dir + RH_SSD_MARSYAS, dir + RH_SSD]
+
+
+for file in files:
+    with open(file, 'r') as inFile:
+        with open(file.rstrip('.csv') + '2.csv', 'w') as outFile:
+            for line in inFile:
+                line = line.split(',')
+                new_line = line[0] + ',' + ','.join(line[2:])
+                outFile.write(new_line)
+
+                # line = line.rstrip()[:-1].split(',')
+                # a = line[0]
+                # b = ','.join(line[1:-1])
+                # c = line[-1]
+                # outFile.write(a + ',' + c + ',' + b + '\n')
